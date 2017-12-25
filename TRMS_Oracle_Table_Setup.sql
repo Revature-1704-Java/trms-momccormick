7c883756707dfe4194ce6dc940137bf2c37eafcc
Matthew McCormick <mastergengar@att.net>
2017-12-25 12:06:51 -0500

    Reorganized query order

--- TRMS_Oracle_Table_Setup.sql
+++ TRMS_Oracle_Table_Setup.sql
@@ -33,8 +33,24 @@ CREATE TABLE Attachments
 (
   ID INT NOT NULL,
   Reimbursement INT NOT NULL,
-  AttachmentPath VARCHAR2(160) UNIQUE NOT NULL,
+  AttachmentPath VARCHAR2(260) UNIQUE NOT NULL, --maxPath
   CONSTRAINT PK_Attachments PRIMARY KEY (ID)
+);
+
+CREATE TABLE ReimbursementNotes
+(
+  ID INT NOT NULL,
+  Reimbursement INT UNIQUE NOT NULL,
+  NoteReason INT NOT NULL,
+  Note VARCHAR2(140) NOT NULL,
+  CONSTRAINT PK_ReimbursementNotes PRIMARY KEY (ID)
+);
+
+CREATE TABLE NoteReasons
+(
+  ID INT NOT NULL,
+  NoteReason VARCHAR2(70) UNIQUE NOT NULL,
+  CONSTRAINT PK_NoteReasons PRIMARY KEY (ID)
 );
 
 CREATE TABLE Reimbursements
@@ -43,56 +59,68 @@ CREATE TABLE Reimbursements
   Employee INT NOT NULL,
   DateSubmitted DATE NOT NULL,
   Event INT UNIQUE NOT NULL,
-  WorkTimeMissed VARCHAR2(160) NOT NULL,
-  Justification VARCHAR2(160) NOT NULL,
+  WorkTimeMissed VARCHAR2(140) NOT NULL,
+  Justification VARCHAR2(140) NOT NULL,
   ProjectedAmount NUMBER(6,2) NOT NULL,
   ReimbursementStatus INT NOT NULL,
   ManagementApprovals INT NOT NULL,
-  AmountAwarded NUMBER(6,2) NOT NULL,
+  AmountAwarded NUMBER(6,2),
   CONSTRAINT PK_Reimbursements PRIMARY KEY (ID)
+);
+
+CREATE TABLE ManagementApprovals
+(
+  ID INT NOT NULL,
+  DirectSupervisor INT,
+  DirectSupervisorDate DATE,
+  DepartmentHead INT,
+  DepartmentHeadDate DATE,
+  BenefitsCoordinator INT,
+  BenefitsCoordinatorDate DATE,
+  CONSTRAINT PK_ManagementApprovals PRIMARY KEY (ID)
 );
 
 CREATE TABLE Employees
 (
   ID INT NOT NULL,
-  FirstName VARCHAR2(160) NOT NULL,
-  LastName VARCHAR2(160) NOT NULL,
-  Email VARCHAR2(160) NOT NULL,
-  Password VARCHAR2(160) NOT NULL,
+  FirstName VARCHAR2(35) NOT NULL,
+  LastName VARCHAR2(35) NOT NULL,
+  Email VARCHAR2(100) NOT NULL, --firstname.lastname@domain
+  Password VARCHAR2(128) NOT NULL,
   EmployeeType INT NOT NULL,
-  DirectSupervisor INT NOT NULL,
-  DepartmentHead INT NOT NULL,
+  DirectSupervisor INT,
+  DepartmentHead INT,
   CONSTRAINT PK_Employees PRIMARY KEY (ID)
 );
 
 CREATE TABLE EmployeeTypes
 (
   ID INT NOT NULL,
-  EmployeeType VARCHAR2(160) UNIQUE NOT NULL,
+  EmployeeType VARCHAR2(100) UNIQUE NOT NULL,
   CONSTRAINT PK_EmployeeTypes PRIMARY KEY (ID)
 );
 
 CREATE TABLE Events
 (
   ID INT NOT NULL,
-  Name VARCHAR2(160) NOT NULL,
+  Name VARCHAR2(100) NOT NULL,
   EventType INT NOT NULL,
-  Description VARCHAR2(160) NOT NULL,
+  Description VARCHAR2(140) NOT NULL,
   StartDate DATE NOT NULL,
   EndDate DATE NOT NULL,
   Time TIMESTAMP NOT NULL,
-  Location VARCHAR2(160) NOT NULL,
+  Location VARCHAR2(175) NOT NULL,
   Cost NUMBER(6,2) NOT NULL,
   GradingFormat INT NOT NULL,
   PassingGrade INT NOT NULL,
-  GradeRecieved NUMBER(4,2) NOT NULL,
+  GradeRecieved NUMBER(4,2),
   CONSTRAINT PK_Events PRIMARY KEY (ID)
 );
 
 CREATE TABLE EventTypes
 (
   ID INT NOT NULL,
-  EventType VARCHAR2(160) UNIQUE NOT NULL,
+  EventType VARCHAR2(70) UNIQUE NOT NULL,
   CostPercentCovered NUMBER(2,1) NOT NULL,
   CONSTRAINT PK_EventTypes PRIMARY KEY (ID)
 );
@@ -100,14 +128,14 @@ CREATE TABLE EventTypes
 CREATE TABLE GradingFormats
 (
   ID INT NOT NULL,
-  GradingFormat VARCHAR2(160) UNIQUE NOT NULL, 
+  GradingFormat VARCHAR2(35) UNIQUE NOT NULL, 
   CONSTRAINT PK_GradingFormats PRIMARY KEY (ID)
 );
 
 CREATE TABLE GradeLetters
 (
   ID INT NOT NULL,
-  GradeLetter VARCHAR2(160) UNIQUE NOT NULL,
+  GradeLetter VARCHAR2(2) UNIQUE NOT NULL,
   MinPercentage NUMBER(2,1) UNIQUE NOT NULL,
   MaxPercentage NUMBER(2,1) UNIQUE NOT NULL,
   CONSTRAINT PK_GradeLetters PRIMARY KEY (ID)
@@ -116,36 +144,8 @@ CREATE TABLE GradeLetters
 CREATE TABLE ReimbursementStatuses
 (
   ID INT NOT NULL,
-  ReimbursementStatus VARCHAR2(160) UNIQUE NOT NULL,
+  ReimbursementStatus VARCHAR2(35) UNIQUE NOT NULL,
   CONSTRAINT PK_ReimbursementStatuses PRIMARY KEY (ID)
-);
-
-CREATE TABLE ManagementApprovals
-(
-  ID INT NOT NULL,
-  DirectSupervisor INT NOT NULL,
-  DepartmentHead INT NOT NULL,
-  BenefitsCoordinator INT NOT NULL,
-  DirectSupervisorDate DATE NOT NULL,
-  DepartmentHeadDate DATE NOT NULL,
-  BenefitsCoordinatorDate DATE NOT NULL,
-  CONSTRAINT PK_ManagementApprovals PRIMARY KEY (ID)
-);
-
-CREATE TABLE ReimbursementNotes
-(
-  ID INT NOT NULL,
-  Reimbursement INT UNIQUE NOT NULL,
-  NoteReason INT NOT NULL,
-  Note VARCHAR2(160) NOT NULL,
-  CONSTRAINT PK_ReimbursementNotes PRIMARY KEY (ID)
-);
-
-CREATE TABLE NoteReasons
-(
-  ID INT NOT NULL,
-  NoteReason VARCHAR2(160) UNIQUE NOT NULL,
-  CONSTRAINT PK_NoteReasons PRIMARY KEY (ID)
 );
 
 
@@ -153,26 +153,28 @@ CREATE TABLE NoteReasons
 *Add Foriegn Key Constraints*
 ****************************/
 ALTER TABLE Attachments ADD CONSTRAINT FK_Reimbursement_1 FOREIGN KEY (Reimbursement) REFERENCES Reimbursements (ID);
-ALTER TABLE Reimbursements ADD CONSTRAINT FK_Employee FOREIGN KEY (Employee) REFERENCES Employees (ID);
+ALTER TABLE ReimbursementNotes ADD CONSTRAINT FK_Reimbursement_2 FOREIGN KEY (Reimbursement) REFERENCES Reimbursements (ID);
+ALTER TABLE ReimbursementNotes ADD CONSTRAINT FK_NoteReason FOREIGN KEY (NoteReason) REFERENCES NoteReasons (ID);ALTER TABLE Reimbursements ADD CONSTRAINT FK_Employee FOREIGN KEY (Employee) REFERENCES Employees (ID);
 ALTER TABLE Reimbursements ADD CONSTRAINT FK_Event FOREIGN KEY (Event) REFERENCES Events (ID);
 ALTER TABLE Reimbursements ADD CONSTRAINT FK_ReimbursementStatus FOREIGN KEY (ReimbursementStatus) REFERENCES ReimbursementStatuses (ID);
 ALTER TABLE Reimbursements ADD CONSTRAINT FK_ManagementApprovals FOREIGN KEY (ManagementApprovals) REFERENCES ManagementApprovals (ID);
+ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_DirectSupervisor_2 FOREIGN KEY (DirectSupervisor) REFERENCES Employees (ID);
+ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_DepartmentHead_2 FOREIGN KEY (DepartmentHead) REFERENCES Employees (ID);
+ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_BenefitsCoordinator FOREIGN KEY (BenefitsCoordinator) REFERENCES Employees (ID);
 ALTER TABLE Employees ADD CONSTRAINT FK_EmployeeType FOREIGN KEY (EmployeeType) REFERENCES EmployeeTypes (ID);
 ALTER TABLE Employees ADD CONSTRAINT FK_DirectSupervisor_1 FOREIGN KEY (DirectSupervisor) REFERENCES Employees (ID);
 ALTER TABLE Employees ADD CONSTRAINT FK_DepartmentHead_1 FOREIGN KEY (DepartmentHead) REFERENCES Employees (ID);
 ALTER TABLE Events ADD CONSTRAINT FK_EventType FOREIGN KEY (EventType) REFERENCES EventTypes (ID);
 ALTER TABLE Events ADD CONSTRAINT FK_GradingFormat FOREIGN KEY (GradingFormat) REFERENCES GradingFormats (ID);
 ALTER TABLE Events ADD CONSTRAINT FK_GradeCutoff FOREIGN KEY (PassingGrade) REFERENCES GradeLetters (ID);
-ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_DirectSupervisor_2 FOREIGN KEY (DirectSupervisor) REFERENCES Employees (ID);
-ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_DepartmentHead_2 FOREIGN KEY (DepartmentHead) REFERENCES Employees (ID);
-ALTER TABLE ManagementApprovals ADD CONSTRAINT FK_BenefitsCoordinator FOREIGN KEY (BenefitsCoordinator) REFERENCES Employees (ID);
-ALTER TABLE ReimbursementNotes ADD CONSTRAINT FK_Reimbursement_2 FOREIGN KEY (Reimbursement) REFERENCES Reimbursements (ID);
-ALTER TABLE ReimbursementNotes ADD CONSTRAINT FK_NoteReason FOREIGN KEY (NoteReason) REFERENCES NoteReasons (ID);
 
 
 /*************************
 *Add Reference Table Data*
 *************************/
+INSERT INTO NoteReasons (ID, NoteReason) VALUES (1,'Reimbursement Amount Exceeded');
+INSERT INTO NoteReasons (ID, NoteReason) VALUES (2,'Reimbursement Denied');
+
 INSERT INTO EmployeeTypes (ID, EmployeeType) VALUES (1,'Standard');
 INSERT INTO EmployeeTypes (ID, EmployeeType) VALUES (2,'Direct Supervisor');
 INSERT INTO EmployeeTypes (ID, EmployeeType) VALUES (3,'Department Head');
@@ -201,9 +203,6 @@ INSERT INTO ReimbursementStatuses (ID, ReimbursementStatus) VALUES (4,'Awarded')
 INSERT INTO ReimbursementStatuses (ID, ReimbursementStatus) VALUES (5,'Canceled');
 INSERT INTO ReimbursementStatuses (ID, ReimbursementStatus) VALUES (6,'Urgent');
 INSERT INTO ReimbursementStatuses (ID, ReimbursementStatus) VALUES (7,'Denied');
-
-INSERT INTO NoteReasons (ID, NoteReason) VALUES (1,'Reimbursement Amount Exceeded');
-INSERT INTO NoteReasons (ID, NoteReason) VALUES (2,'Reimbursement Denied');
 
 
 /********************************************
