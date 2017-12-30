66b67bb5430ff11b2a2df1506ac38735d1440607
Matthew McCormick <mastergengar@att.net>
2017-12-30 17:49:08 -0500

    Removed to reflect new database setup

--- TRMS/TRMS/src/main/java/daoInterfaces/ApprovalDatesDaoInterface.java
+++
@@ -1,8 +0,0 @@
-package daoInterfaces;
-
-import daoObjects.ApprovalDates;
-
-public interface ApprovalDatesDaoInterface extends BaseDaoInterface<ApprovalDates> {
-
-
-}
--- TRMS/TRMS/src/main/java/daoInterfaces/GradesDaoInterface.java
+++
@@ -1,8 +0,0 @@
-package daoInterfaces;
-
-import daoObjects.Grades;
-
-public interface GradesDaoInterface extends BaseDaoInterface<Grades> {
-
-
-}
