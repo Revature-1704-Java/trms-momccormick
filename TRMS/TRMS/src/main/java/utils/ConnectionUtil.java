1ce036abfd9f135065291a2f1eb63bade582d862
Matthew McCormick <mastergengar@att.net>
2018-01-01 12:58:14 -0500

    Updated so that jdbc driver should be auto detected

--- TRMS/TRMS/src/main/java/utils/ConnectionUtil.java
+++ TRMS/TRMS/src/main/java/utils/ConnectionUtil.java
@@ -6,12 +6,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
+
+	private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private static String username = "trns";
+	private static String password = "pass";
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.getStackTrace();
+		}
+	}
+	
 	public static Connection getConnection() throws SQLException, IOException {
-
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "trns";
-		String password = "pass";
-
-		return DriverManager.getConnection(url, user, password);
+		return DriverManager.getConnection(url, username, password);
 	}
 }
