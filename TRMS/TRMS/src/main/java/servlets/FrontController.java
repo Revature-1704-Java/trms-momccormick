d057f79849c7018e4800b167253ccad2e9c971af
Matthew McCormick <mastergengar@att.net>
2018-01-01 13:01:54 -0500

    More trouble than necessary.  CUrrently redacting

--- TRMS/TRMS/src/main/java/servlets/FrontController.java
+++ TRMS/TRMS/src/main/java/servlets/FrontController.java
@@ -7,21 +7,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-@WebServlet("/")
+@WebServlet("")
 public class FrontController extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-//		HttpSession session = request.getSession();
-//		
-//		String username = (String) session.getAttribute("username");
-//		
-//		if(username == null) {
-			response.sendRedirect("index");
-//		}
-		
+		System.out.println(request.getRequestURI());
+		response.sendRedirect("login");
+//		request.getRequestDispatcher("LoginServlet").forward(request, response);;
 	}
 
 }
