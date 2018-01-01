97d19d2dc01a9fe975ce2763acf3c88515f3004e
Matthew McCormick <mastergengar@att.net>
2018-01-01 13:00:14 -0500

    Basic Login set up

--- TRMS/TRMS/src/main/java/servlets/LoginServlet.java
+++ TRMS/TRMS/src/main/java/servlets/LoginServlet.java
@@ -7,13 +7,41 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-@WebServlet("/login/login")
+import daoObjects.Employee;
+import daos.EmployeeDao;
+import utils.SessionAttribute;
+
+//@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.sendRedirect("login.jsp");
+		
+		System.out.println(request.getRequestURI());
+		response.sendRedirect("login.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		EmployeeDao empDao = new EmployeeDao();
+		Employee emp = empDao.getEmployeeWithLogin(email, password);
+
+		System.out.println(email + " " + password);
+		System.out.println(emp == null ? "NULL" : emp.toString());
+
+		if (emp != null) {
+			HttpSession session = request.getSession();
+			session.setAttribute(SessionAttribute.EMPLOYEE, emp);
+
+			response.sendRedirect("WEB-INF/viewReimbursements.html");
+		}
+
+		response.getWriter().append("Login Failed");
 	}
 }
